0e9d7db6b19d365f32f09ac702824f22f93c56f1
Dyuryagin Sergey <dyuryagin@gmail.com>
2022-04-12 11:25:47 +0300

    snake 20

---
+++ snake_game/snake.py
@@ -0,0 +1,33 @@
+from turtle import Turtle
+
+
+class Snake:
+
+    def __init__(self):
+        self.starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+        self.segments = []
+        for position in self.starting_positions:
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segments.append(new_segment)
+
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.segments[0].forward(20)
+
+    def up(self):
+        self.segments[0].setheading(90)
+
+    def down(self):
+        self.segments[0].setheading(270)
+
+    def left(self):
+        self.segments[0].setheading(180)
+
+    def right(self):
+        self.segments[0].setheading(0)
