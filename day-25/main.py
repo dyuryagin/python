38da3be636a005ed7802ff4d1f140444526704cb
Dyuryagin Sergey <dyuryagin@gmail.com>
2022-04-25 10:17:12 +0300

    25

--- day-25/main.py
+++ day-25/main.py
@@ -7,9 +7,24 @@
 #         if row[1] != "temp":
 #             temperature.append(int(row[1]))
 #
+#
+# import pandas
+#
+# data = pandas.read_csv("weather_data.csv")
+# print(data[data.day == "Monday"])
+#
 
 import pandas
 
-data = pandas.read_csv("weather_data.csv")
-print(data[data.day == "Monday"])
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+gray_squirrels_count = len(data[data['Primary Fur Color'] == "Gray"])
+red_squirrels_count = len(data[data['Primary Fur Color'] == "Cinnamon"])
+black_squirrels_count = len(data[data['Primary Fur Color'] == "Black"])
 
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [gray_squirrels_count, red_squirrels_count, black_squirrels_count]
+}
+
+df = pandas.DataFrame(data_dict)
+df.to_csv("squirrel_count.csv")
